abe120ce93029b02b9b34f29be4615e430a9d8c5
jnahelou <nahelou.j@sfeir.com>
2023-11-22 14:53:46 +0100

    feat(resource_job_template_launch): add more configuration options (limit, inventory, completion wait)

--- awx/resource_job_template_launch.go
+++ awx/resource_job_template_launch.go
@@ -29,12 +29,15 @@ package awx
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"strconv"
+	"time"
 
 	awx "github.com/denouche/goawx/client"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
@@ -51,33 +54,107 @@ func resourceJobTemplateLaunch() *schema.Resource {
 				Description: "Job template ID",
 				ForceNew:    true,
 			},
+			"limit": {
+				Type:        schema.TypeString,
+				Required:    false,
+				Optional:    true,
+				ForceNew:    true,
+				Description: "List of comma delimited hosts to limit job execution. Required ask_limit_on_launch set on job_template.",
+			},
+			"inventory": {
+				Type:        schema.TypeInt,
+				Required:    false,
+				Optional:    true,
+				Default:     "",
+				Description: "Override Inventory ID. Required ask_inventory_on_launch set on job_template.",
+				ForceNew:    true,
+			},
+			"wait_for_completion": {
+				Type:        schema.TypeBool,
+				Required:    false,
+				Optional:    true,
+				Default:     false,
+				Description: "Resource creation will wait for job completion.",
+				ForceNew:    true,
+			},
 		},
 	}
+}
+
+func statusInstanceState(ctx context.Context, svc *awx.JobService, id int) retry.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		output, err := svc.GetJob(id, map[string]string{})
+		return output, output.Status, err
+	}
+}
+
+func jobTemplateLaunchWait(ctx context.Context, svc *awx.JobService, job *awx.JobLaunch, timeout time.Duration) error {
+
+	stateConf := &retry.StateChangeConf{
+		Pending:    []string{"new", "pending", "waiting", "running"},
+		Target:     []string{"successful"},
+		Refresh:    statusInstanceState(ctx, svc, job.ID),
+		Timeout:    timeout,
+		Delay:      10 * time.Second,
+		MinTimeout: 3 * time.Second,
+	}
+
+	_, err := stateConf.WaitForStateContext(ctx)
+
+	return err
+}
+
+// JobTemplateLaunchData provides payload data used by the JobTemplateLaunch method
+type JobTemplateLaunchData struct {
+	Limit     string `json:"limit,omitempty"`
+	Inventory int    `json:"inventory,omitempty"`
 }
 
 func resourceJobTemplateLaunchCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
 	client := m.(*awx.AWX)
 	awxService := client.JobTemplateService
+	awxJobService := client.JobService
+
 	jobTemplateID := d.Get("job_template_id").(int)
 	_, err := awxService.GetJobTemplateByID(jobTemplateID, make(map[string]string))
 	if err != nil {
 		return buildDiagNotFoundFail("job template", jobTemplateID, err)
 	}
 
-	res, err := awxService.Launch(jobTemplateID, map[string]interface{}{}, map[string]string{})
+	data := JobTemplateLaunchData{
+		Limit:     d.Get("limit").(string),
+		Inventory: d.Get("inventory").(int),
+	}
+
+	var iData map[string]interface{}
+	idata, _ := json.Marshal(data)
+	json.Unmarshal(idata, &iData)
+
+	res, err := awxService.Launch(jobTemplateID, iData, map[string]string{})
 	if err != nil {
 		log.Printf("Failed to create Template Launch %v", err)
 		diags = append(diags, diag.Diagnostic{
 			Severity: diag.Error,
 			Summary:  "Unable to create JobTemplate",
-			Detail:   fmt.Sprintf("JobTemplate with name %s in the project id %d, failed to create %s", d.Get("name").(string), d.Get("project_id").(int), err.Error()),
+			Detail:   fmt.Sprintf("JobTemplateLaunch with template ID %d, failed to create %s", d.Get("job_template_id").(int), err.Error()),
 		})
 		return diags
 	}
 
 	// return resourceJobRead(ctx, d, m)
 	d.SetId(strconv.Itoa(res.ID))
+
+	if d.Get("wait_for_completion").(bool) {
+		err = jobTemplateLaunchWait(ctx, awxJobService, res, d.Timeout(schema.TimeoutCreate))
+		if err != nil {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Error,
+				Summary:  "JobTemplate execution failure",
+				Detail:   fmt.Sprintf("JobTemplateLaunch with template ID %d, failed to complete %s", d.Get("job_template_id").(int), err.Error()),
+			})
+		}
+	}
 	return diags
 }
 
--- go.mod
+++ go.mod
@@ -3,9 +3,18 @@ module github.com/denouche/terraform-provider-awx
 go 1.14
 
 require (
+	github.com/Masterminds/semver v1.5.0 // indirect
+	github.com/Masterminds/sprig v2.22.0+incompatible // indirect
+	github.com/andybalholm/crlf v0.0.0-20171020200849-670099aa064f // indirect
+	github.com/apparentlymart/go-cidr v1.0.1 // indirect
+	github.com/apparentlymart/go-dump v0.0.0-20190214190832-042adf3cf4a0 // indirect
 	github.com/denouche/goawx v0.18.1
+	github.com/flynn/go-shlex v0.0.0-20150515145356-3f9db97f8568 // indirect
 	github.com/gruntwork-io/terratest v0.31.2
-	github.com/hashicorp/terraform-plugin-sdk/v2 v2.10.1
-	github.com/stretchr/testify v1.7.0
+	github.com/hashicorp/go-getter v1.5.3 // indirect
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.30.0
+	github.com/nsf/jsondiff v0.0.0-20200515183724-f29ed568f4ce // indirect
+	github.com/stretchr/testify v1.8.3
+	github.com/vmihailenco/msgpack/v4 v4.3.12 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 )
